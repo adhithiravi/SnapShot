424846f9b583aa715e5a5413491442d8befbeb5f
Adhithi Ravichandran <adhithi@redivus.com>
2021-08-16 14:07:13 -0500

    Object destructure refactor

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,12 +8,8 @@ const Gallery = props => {
   // map variables to each item in fetched image array and return image component
   if (results.length > 0) {
     images = results.map(image => {
-      let farm = image.farm;
-      let server = image.server;
-      let id = image.id;
-      let secret = image.secret;
-      let title = image.title;
-      let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
+      const { farm, server, id, secret, title} = image;
+      const url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
       return <Image url={url} key={id} alt={title} />;
     });
   } else {
