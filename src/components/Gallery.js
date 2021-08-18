50b30ad3506935388e22de0432b06dc2c1ac9ab0
Adhithi Ravichandran <adhithi@redivus.com>
2021-08-18 13:35:14 -0500

    Remove gallery for workshop excersice

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,24 +1,7 @@
-import React, { useContext, useEffect } from "react";
-import { PhotoContext } from "../context/PhotoContext";
-import ImageList from "./ImageList";
-import Loader from "./Loader";
+import React from "react";
 
-function Gallery({ searchTerm }) {
-  const { images, loading, runSearch } = useContext(PhotoContext);
-
-  /**  Data fetching is done here since it is a side-effect 
-   Everytime the searchTerm changes, the useEffect will be invoked and
-  run the search again */
-  useEffect(() => {
-    runSearch(searchTerm);
-    // eslint-disable-next-line
-  }, [searchTerm]);
-
-  return (
-    <div className="photo-container">
-      {loading ? <Loader /> : <ImageList data={images} />}
-    </div>
-  );
+function Gallery() {
+  return <div className="photo-container"></div>;
 }
 
 export default Gallery;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,19 +2,17 @@ import React from "react";
 import NoImage from "./NoImage";
 import Image from "./Image";
 
-function ImageList(props) {
-  const results = props.data;
-  let images;
-
-  if (results.length > 0) {
-    images = results.map((image) => {
+function ImageList({ images }) {
+  let imageList;
+  if (images.length > 0) {
+    imageList = images.map((image) => {
       const { farm, server, id, secret, title } = image;
       const url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
       return <Image url={url} key={id} alt={title} />;
     });
   }
 
-  return images ? <ul>{images}</ul> : <NoImage />;
+  return imageList ? <ul>{imageList}</ul> : <NoImage />;
 }
 
 export default ImageList;
